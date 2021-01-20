d647963dfa3916b307519364f029ca55520bd254
Amine L'ch <aminelch95@gmail.com>
2021-01-20 22:32:28 +0100

    creating Gravatar class

--- src/Gravatar.php
+++ src/Gravatar.php
@@ -1,13 +1,62 @@
-<?php 
+<?php
 
-namespace App; 
+    namespace Gravatar;
+
+    /**
+     * Class Gravatar
+     * this class is a simple implementation of gravatar service
+     * @author aminelch
+     * @package Gravatar
+     */
+    class Gravatar
+    {
+        /**
+         * @var string
+         */
+        private  $email;
 
 
-class Avatar {
+        /**
+         * Gravatar constructor.
+         *
+         * @param string $email
+         */
+        public function __construct(string $email)
+        {
+            $this->email = $email;
+
+        }
+
+        /**
+         * get a md5 hash for an email
+         * @param $email
+         *
+         * @return string
+         */
+        public function hash($email):string {
+
+            return md5(strtolower(trim($email)));
+
+        }
+        /**
+         * @return string
+         */
+        public function getEmail(): string
+        {
+            return $this->email;
+        }
+
+        /**
+         * @param string $email
+         */
+        public function setEmail(string $email): void
+        {
+            $this->email = $email;
+        }
 
 
 
 
 
 
-}
+    }
